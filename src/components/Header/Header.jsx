500ffe0a557174b36d623c2eeed4ccd43deea7aa
Eduard-Konovka <ed098ua@gmail.com>
2022-04-03 18:31:33 +0300

    Added plug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
-import Logo from 'components/Logo';
-// import AuthNav from 'components/AuthNav';
+import {Logo} from 'components/Logo/Logo';
 import s from './Header.module.css';
 
 export default function Header() {
   return (
     <header className={s.header}>
-      <Logo/>
-      <div>AuthNav</div>
+      <Logo />
+
+      <div className={s.nav}>
+        Name | Log out
+      </div>
     </header>
   );
 }
