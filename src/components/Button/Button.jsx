cbb1ce34e2e1bc020e91c7b30088166857927a2a
KonstantinZakrzhevskyi <konstantinz2505@gmail.com>
2022-04-03 17:36:01 +0300

    bugfix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import s from "./Button.module.css";
 
-function Button({className, onClick, type, disabled = false, children }) {
+function Button({className, onClick, type, disabled = false, text }) {
   return (
-    <button className={s[className]} onClick={onClick} type={type} disabled={disabled}>{children}</button>
+    <button className={s[className]} onClick={onClick} type={type} disabled={disabled}>{text}</button>
     )
 }
 
@@ -12,7 +12,7 @@ Button.prototypes = {
   onClick: PropTypes.func,
   type: PropTypes.string.isRequired,
   disabled: PropTypes.bool,
-  children: PropTypes.string,
+  text: PropTypes.string,
 }
 
 export default Button;
