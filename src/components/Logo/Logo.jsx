9ae345daf28984310a0baad341aaaf135cc9ff90
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2022-04-03 15:12:05 +0300

    logo component added

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import s from './Logo.module.css';
+import { ReactComponent as ReactSprite } from 'images/sprite.svg';
+
+export function Logo() {
+  return (
+    <span className={s.logo}>
+      <svg className={s.logoIcon}>
+        <use href='#icon-logo'></use>
+      </svg>
+      <span className={s.logoText}>Wallet</span>
+    </span>
+  );
+}
