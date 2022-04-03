22944f8dd89de858fcbee5f0980dd56a3db426d4
andrii-svyshch <80419679+andrii-svyshch@users.noreply.github.com>
2022-04-03 20:59:51 +0300

    addTransactions button and modal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useState } from 'react';
 import Loading from 'components/Loading';
+import ButtonAddTransactions from 'components/ButtonAddTransactions';
+import Modal from 'components/Modal';
+import ModalAddTransaction from 'components/ModalAddTransaction';
 
 const DashboardPage = lazy(() =>
   import('pages/DashboardPage' /* webpackChunkName: "dashboard-page" */),
 );
 
 function App() {
+  const [showModalAddTransaction, setShowModalAddTransaction] = useState(false);
+
+  const toggleModalAddTransaction = () => {
+    setShowModalAddTransaction(showModalAddTransaction => !showModalAddTransaction);
+  };
   return (
-    <Suspense fallback={<Loading />}>
-      <DashboardPage />
-    </Suspense>
+    <>
+      <Suspense fallback={<Loading />}>
+        <DashboardPage />
+      </Suspense>
+      <ButtonAddTransactions onClick={toggleModalAddTransaction} />
+      {showModalAddTransaction && (
+        <Modal onClose={toggleModalAddTransaction}>
+          <ModalAddTransaction
+            onClose={toggleModalAddTransaction}
+            onSubmit={(amount, date) => console.log(`Amount-${amount}. Date-${date}`)}
+          />
+        </Modal>
+      )}
+    </>
   );
 }
 
