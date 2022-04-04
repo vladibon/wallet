1763988665688804ae031335f92ac0938cd2eb40
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2022-04-04 10:45:26 +0200

    delete from app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Suspense, lazy, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ButtonAddTransactions from 'components/ButtonAddTransactions';
-import { DoughnutChart } from 'components/DoughnutChart/DoughnutChart';
 import Modal from 'components/Modal';
 import ModalAddTransaction from 'components/ModalAddTransaction';
 import { setUser, resetUser, openModal, closeModal } from './redux';
@@ -33,7 +32,6 @@ function App() {
       <button onClick={() => dispatch(openModal())}>show</button>
       <button onClick={() => dispatch(closeModal())}>hide</button>
       {isModalOpen && <p>MODAL</p>}
-      <DoughnutChart />
       {/* Andrii */}
       <ButtonAddTransactions onClick={toggleModalAddTransaction} />
       {showModalAddTransaction && (
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,7 +1,9 @@
 import s from './DashboardPage.module.css';
-
+import { DoughnutChart } from 'components/DoughnutChart/DoughnutChart';
 function DashboardPage() {
-  return <main className={s.main}>Main</main>;
+  return <main className={s.main}>Main
+  <DoughnutChart />
+  </main>;
 }
 
 export default DashboardPage;
