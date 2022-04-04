c6629697d185370e72abdb02f6ab10e97af0b7d5
vofm <valerafm.vofm@gmail.com>
2022-04-04 21:57:07 +0300

    Update

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -3,43 +3,39 @@ import s from './HomeTab.module.css';
 
 function HomeTab({ items }) {
   return (
-    <>
-      <ul className={s.homeTab}>
-        <li className={s.homeTab__hidden}>
-          <ul className={s.homeTab__title}>
-            <li className={s.homeTab__titleCell}>Дата</li>
-            <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellCenter}`}>Тип</li>
-            <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellWidth}`}>
-              Категория
-            </li>
-            <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellWidth}`}>
-              Комментарий
-            </li>
-            <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellEnd}`}>Сумма</li>
-            <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellEnd}`}>Баланс</li>
-          </ul>
-        </li>
-        {items.map(item => (
-          <li
-            className={
-              item.type === '+'
-                ? `${s.homeTab__items}  ${s.income}`
-                : `${s.homeTab__items}  ${s.expense}`
-            }
-            key={item.id}
-          >
-            <HomeTabItems
-              date={item.date}
-              type={item.type}
-              category={item.category}
-              comment={item.comment}
-              sum={item.sum}
-              balance={item.balance}
-            />
+    <ul className={s.homeTab}>
+      <li className={s.homeTab__hidden}>
+        <ul className={s.homeTab__title}>
+          <li className={s.homeTab__titleCell}>Дата</li>
+          <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellCenter}`}>Тип</li>
+          <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellWidth}`}>Категория</li>
+          <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellWidth}`}>
+            Комментарий
           </li>
-        ))}
-      </ul>
-    </>
+          <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellEnd}`}>Сумма</li>
+          <li className={`${s.homeTab__titleCell} ${s.homeTabItems__titleCellEnd}`}>Баланс</li>
+        </ul>
+      </li>
+      {items.map(item => (
+        <li
+          className={
+            item.type === '+'
+              ? `${s.homeTab__items}  ${s.income}`
+              : `${s.homeTab__items}  ${s.expense}`
+          }
+          key={item.id}
+        >
+          <HomeTabItems
+            date={item.date}
+            type={item.type}
+            category={item.category}
+            comment={item.comment}
+            sum={item.sum}
+            balance={item.balance}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
 
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -6,12 +6,7 @@ import s from './DashboardPage.module.css';
 function DashboardPage() {
   return (
     <main className={s.main}>
-      {transactions ? (
-        <HomeTab items={transactions} />
-      ) : (
-          
-        // <img className={s.homeTab__img} src='../../images/home-tab-bg.png' alt='Transactions'></img>
-      )}
+      <HomeTab items={transactions} />
     </main>
   );
 }
