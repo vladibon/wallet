8efe8a2ffff52a8d9cd37a30aa6c98da0e967393
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2022-04-07 17:00:22 +0300

    state

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -5,13 +5,28 @@ import Icons from 'images/sprite.svg';
 import { Link } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { useLogInUserMutation, setUser } from 'redux/index';
 
 export default function LoginForm() {
   const [loginUser, { data, error }] = useLogInUserMutation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const dispatch = useDispatch();
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return;
+    }
+  };
 
   useEffect(() => {
     if (data) {
@@ -32,21 +47,34 @@ export default function LoginForm() {
       <div className={s.logo}>
         <Logo />
       </div>
-      <form className={s.form}>
+      <form className={s.form} onSubmit={onLoginSubmit}>
         <label className={s.authLabel}>
-          <input className={s.input} placeholder='E-mail' name='email'></input>
+          <input
+            className={s.input}
+            placeholder='E-mail'
+            onChange={handleChange}
+            name='email'
+            value={email}
+          ></input>
           <svg width='21' height='16' className={s.inputIcon}>
             <use href={`${Icons}#icon-email`} />
           </svg>
         </label>
 
         <label className={s.authLabel}>
-          <input className={s.input} placeholder='Password' name='password' type='password'></input>
+          <input
+            className={s.input}
+            placeholder='Password'
+            onChange={handleChange}
+            name='password'
+            type='password'
+            value={password}
+          ></input>
           <svg width='16' height='21' className={s.inputIcon}>
             <use href={`${Icons}#icon-lock`} />
           </svg>
         </label>
-        <button className={s.logBtn} type='button' onClick={onLoginSubmit}>
+        <button className={s.logBtn} type='submit'>
           log in
         </button>
         <Link to='/register' className={s.authLink}>
