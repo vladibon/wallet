4208691b0b97b3845f853c9808f0c2e4fd35c46b
Eduard-Konovka <ed098ua@gmail.com>
2022-04-07 21:59:38 +0300

    Replaces the <div> tag with a <button>

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ModalLogout from 'components/ModalLogout';
 import ModalAddTransaction from 'components/ModalAddTransaction';
 import { selectIsModalLogoutOpen, selectIsModalAddTransactionOpen } from 'redux/selectors';
 import SaveCategoriesHook from 'hooks/SaveCategoriesHook';
+import Header from 'components/Header';
 
 const DashboardPage = lazy(() =>
   import('pages/DashboardPage' /* webpackChunkName: "dashboard-page" */),
@@ -30,6 +31,7 @@ function App() {
 
   return (
     <>
+      <Header />
       {!isFetching ? (
         <>
           <Suspense fallback={null}>
