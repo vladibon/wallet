9a4500e3035da7ded48cf45bda6fc3d275ebf6b9
vofm <valerafm.vofm@gmail.com>
2022-04-08 07:09:46 +0300

    Add style table

--- src/components/HomeTab/HomeTabDesktop.jsx
+++ src/components/HomeTab/HomeTabDesktop.jsx
@@ -20,23 +20,31 @@ function HomeTabDesktop() {
       <table className={s.table}>
         <thead className={s.tableThead}>
           <tr>
-            <th>Date</th>
+            <th align='left'>Date</th>
             <th>Type</th>
-            <th>Category</th>
-            <th>Comment</th>
-            <th>Amount</th>
-            <th>Balance</th>
+            <th align='left'>Category</th>
+            <th align='left'>Comment</th>
+            <th align='right'>Amount</th>
+            <th align='right'>Balance</th>
           </tr>
         </thead>
         <tbody>
           {transactions.map(transaction => (
             <tr>
               <td>{transaction.date}</td>
-              <td>{transaction.type}</td>
+              <td align='center'>{transaction.type ? '+' : '-'}</td>
               <td>{transaction.category}</td>
               <td>{transaction.comment}</td>
-              <td>{transaction.amount}</td>
-              <td>{transaction.balance}</td>
+              <td className={transaction.type ? s.income : s.expense} align='right'>
+                {Intl.NumberFormat('ru-Ru', { minimumFractionDigits: 2 }).format(
+                  transaction.amount,
+                )}
+              </td>
+              <td align='right'>
+                {Intl.NumberFormat('ru-Ru', { minimumFractionDigits: 2 }).format(
+                  transaction.balance,
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
