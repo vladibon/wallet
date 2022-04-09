b1a3281cb1867259e05c2c3b1b0cd90c6f29fd55
KonstantinZakrzhevskyi <konstantinz2505@gmail.com>
2022-04-09 14:10:57 +0300

    add localstorage

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -7,14 +7,49 @@ import s from './Currency.module.css';
 function Currency() {
   const [exchanges, setExchanges] = useState([]);
 
+  // useEffect(() => {
+
+  //   fetchCurrency()
+  //     .then(data => {
+  //       // setExchanges([...data]);
+  //       // localStorage.setItem("exchanges", JSON.stringify(data));
+  //       // setExchanges(JSON.parse(localStorage.getItem("exchanges")));
+  //     })
+  //     .catch(error => {
+  //       throw Error;
+  //     });
+  // }, []);
+
+
+// =====================
+
   useEffect(() => {
     fetchCurrency()
       .then(data => {
+      //   const currencyArray = [...data];
+      //   return currencyArray;
+      // })
+      // .then(currencyArray => {
         setExchanges([...data]);
+        localStorage.setItem("exchanges", JSON.stringify(data));
+        localStorage.setItem("exchangesTime", JSON.stringify(Date.now()));
+
+        const exchangesLS = JSON.parse(localStorage.getItem("exchanges"));
+        const exchangesTime = JSON.parse(localStorage.getItem("exchangesTime"));
+        
+        if (!exchangesLS) {
+          fetchCurrency();
+        }
+        if (Date.now() - exchangesTime > 3600000) {
+          fetchCurrency();
+          console.log(fetchCurrency());
+        } else {
+          setExchanges(exchangesLS);
+      }
       })
       .catch(error => {
-        throw Error;
-      });
+      throw Error(error);
+    })
   }, []);
 
   return (
