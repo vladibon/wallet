f109e2ddfa38520ba2dc257106b1c5ef5b2feb5e
Eduard-Konovka <ed098ua@gmail.com>
2022-04-10 19:02:27 +0300

    Connects button
    Подключил кнопку страницы и добавил лоадер в PersistGate

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Button from 'components/Button';
 import sound from './glassBreak.mp3';
 import s from './NotFoundPage.module.css';
@@ -16,15 +16,13 @@ export default function NotFoundPage() {
           Error 404:
         </span>
 
-        <br /> Page not found :(
+        <br />
+          Page not found :(
       </p>
 
-      <Button
-        className='btn__primary-logout'
-        onClick={() => <Navigate to='/home' />}
-        type='button'
-        text='Go home'
-      />
+      <NavLink to='/home'>
+        <Button className='btn__primary' onClick={null} type='button' text='Back to Homepage' /> 
+      </NavLink>
     </div>
   );
 }
