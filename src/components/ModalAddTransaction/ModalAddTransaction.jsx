9b98f1da4bdc0efe0317aab44d7716ada38c426d
Caraset <vakabo@gmail.com>
2022-04-13 02:40:53 +0300

    fix failed merge

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Select from 'react-select';
 import { useDispatch, useSelector } from 'react-redux';
+import { validate } from 'indicative/validator';
 import { toast } from 'react-toastify';
 
 import Datetime from 'react-datetime';
@@ -22,14 +23,23 @@ import { setCurrentDate } from './setCurrentDate';
 import Button from 'components/Button';
 import { selectionStyles } from './index.js';
 
+const rules = {
+  category: 'required',
+  comment: 'string|max:40',
+};
+
+const messages = {
+  required: field => `${field} is required`,
+  'comment.max': 'only 40 characters allowed',
+};
+
 export default function ContactForm() {
   const dispatch = useDispatch();
   const { income, expense } = useSelector(selectCategories);
-
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [type, setType] = useState(false);
-  const [category, setCategory] = useState('default');
   const [categories, setCategories] = useState([]);
+
+  const [type, setType] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
   const [amount, setAmount] = useState('');
   const currentDate = setCurrentDate();
   const [date, setDate] = useState(new Date());
@@ -37,15 +47,10 @@ export default function ContactForm() {
 
   const [addTransaction, { data, error, isLoading }] = useAddTransactionMutation();
   const options = categories.map(category => ({ value: category, label: category }));
+  const [validationError, setValidationError] = useState({ field: null, message: '' });
 
   useEffect(() => {
-    if (type) {
-      setCategories(income);
-      setCategory(income[0]);
-    } else {
-      setCategories(expense);
-      setCategory(expense[0]);
-    }
+    type ? setCategories(income) : setCategories(expense);
   }, [expense, income, type]);
 
   useEffect(() => {
@@ -53,12 +58,13 @@ export default function ContactForm() {
       dispatch(setBalance({ balance: data.balance }));
       dispatch(setLatestTransactions(data.transactions));
       dispatch(closeModalWindow());
+      toast.success('Transaction added');
       reset();
     } else if (error) {
       try {
-        if (error.data?.message === 'Balance cannot be negative')
-          toast.error("sorry, you don't have enough money for this expense");
-        else toast.error(error.data?.message || 'your request failed');
+        if (error.data.message === 'Balance cannot be negative')
+          toast.error("Sorry, you don't have enough money for this expense");
+        else toast.error(error.data.message);
       } catch {
         dispatch(setError(500));
         dispatch(closeModalWindow());
@@ -67,8 +73,8 @@ export default function ContactForm() {
   }, [data, dispatch, error]);
 
   const handleChange = selectedOption => {
+    setValidationError({ field: null, message: '' });
     setSelectedOption(selectedOption);
-    setCategory(selectedOption.value);
   };
 
   const SelectCategory = () => (
@@ -78,23 +84,22 @@ export default function ContactForm() {
       defaultValue={selectedOption}
       onChange={handleChange}
       options={options}
+      name='category'
     />
   );
 
   const handleInputChange = e => {
     const { name, value, checked } = e.target;
+    setValidationError({ field: null, message: '' });
 
     switch (name) {
       case 'type':
         setType(checked);
+        setSelectedOption(null);
         break;
 
       case 'amount':
         setAmount(value);
-        break;
-
-      case 'category':
-        setCategory(value);
         break;
 
       case 'date':
@@ -112,7 +117,7 @@ export default function ContactForm() {
 
   const reset = () => {
     setType(false);
-    setCategory('default');
+    setSelectedOption(null);
     setAmount('');
     setDate('');
     setComment('');
@@ -121,21 +126,29 @@ export default function ContactForm() {
   const handleSubmit = e => {
     e.preventDefault();
 
-    const transaction = {
-      date,
-      type,
-      category,
-      comment,
-      amount,
-    };
-
-    addTransaction({ transaction });
+    validate({ category: selectedOption?.value, comment }, rules, messages)
+      .then(() => {
+        const transaction = {
+          date,
+          type,
+          category: selectedOption.value,
+          comment,
+          amount,
+        };
+        addTransaction({ transaction });
+      })
+      .catch(errors => {
+        setValidationError({ field: errors[0].field, message: errors[0].message });
+      });
   };
+
+  const { message, field } = validationError;
 
   return (
     <div className={s.modal}>
       <p className={s.title}>Add transaction</p>
       <form className={s.form} onSubmit={handleSubmit}>
+        {field && <p className={s[`${field}Error`]}>{message}</p>}
         <div className={s.formCheckbox}>
           <span
             className={s.formCheckbox__label}
@@ -192,13 +205,14 @@ export default function ContactForm() {
             initialValue={new Date()}
             closeOnSelect={true}
           />
-          <svg width='24' height='24' className={s.inputIcon}>
-            <use href={`${Icons}#icon-calendar`} />
-          </svg>
         </div>
+        <svg width='24' height='24' className={s.inputIcon}>
+          <use href={`${Icons}#icon-calendar`} />
+        </svg>
 
         <textarea
           className={s.formComent}
+          type='textarea'
           name='comment'
           value={comment}
           onChange={handleInputChange}
