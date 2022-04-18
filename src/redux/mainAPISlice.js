8a551dd90b3f095a1b43882b58d15ca88324bedd
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2022-04-18 22:49:47 +0300

    dependencies updated

--- src/redux/mainAPISlice.js
+++ src/redux/mainAPISlice.js
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const emptySplitApi = createApi({
   baseQuery: fetchBaseQuery({
     // baseUrl: 'https://wallet-proj.herokuapp.com/api',
-    // baseUrl: 'https://wallet-proj.osc-fr1.scalingo.io/api',
-    baseUrl: 'http://localhost:4000/api',
+    baseUrl: 'https://wallet-proj.osc-fr1.scalingo.io/api',
+    // baseUrl: 'http://localhost:4000/api',
 
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth?.token;
