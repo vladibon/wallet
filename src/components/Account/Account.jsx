ee675bb9a3750fea15e5ef45ab32930518393bd6
vofm <valerafm.vofm@gmail.com>
2022-04-18 20:04:19 +0300

    Add adaptive desktop

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -12,72 +12,75 @@ function Account() {
       <h2 className={s.accountTitle}>Account settings</h2>
 
       <div className={s.accountForm__wrapper}>
-        <img className={s.accountImg} src={AccountImg} width='200' height='220' alt='Avatar' />
+        <div>
+          <img className={s.accountImg} src={AccountImg} width='200' height='220' alt='Avatar' />
+          <label class={s.inputFile__button}>
+            <input className={s.inputFile} type='file' name='foto'></input>
+            <div class={s.inputFile__wrapper}>
+              <span className={s.inputFile__iconWrapper}>
+                <MdOutlineFileUpload class={s.inputFile__icon} />
+              </span>
+              <span className={s.inputFile__title}>upload photo</span>
+            </div>
+          </label>
+        </div>
 
-        <label class={s.inputFile__button}>
-          <input className={s.inputFile} type='file' name='foto'></input>
-          <div class={s.inputFile__wrapper}>
-            <span className={s.inputFile__iconWrapper}>
-              <MdOutlineFileUpload class={s.inputFile__icon} />
-            </span>
-            <span className={s.inputFile__title}>upload photo</span>
+        <div>
+          <p className={s.subscription__wrapper}>
+            <MdWork />
+            <span className={s.subscriptionTitle}>Regular subscription</span>
+          </p>
+
+          <div>
+            <label className={s.accountLabel}>
+              <input
+                className={`${s.accountInput} ${s.accountInput__marginBottom}`}
+                placeholder='Name'
+                // onChange={handleChange}
+                name='name'
+                // value={name}
+                autoFocus
+                autoComplete='false'
+              ></input>
+              <button className={s.accountBtn__update}>update</button>
+              <div className={s.accountPlaceholder}>
+                <div className={s.accountPlaceholder__iconWrapper}>
+                  <svg width='21' height='16'>
+                    <use href={`${Icons}#icon-account_box`} />
+                  </svg>
+                  <span className={s.accountPlaceholder__title}>Name</span>
+                </div>
+              </div>
+            </label>
           </div>
-        </label>
 
-        <p className={s.subscription__wrapper}>
-          <MdWork />
-          <span className={s.subscriptionTitle}>Regular subscription</span>
-        </p>
-
-        <div>
-          <label className={s.accountLabel}>
-            <input
-              className={`${s.accountInput} ${s.accountInput__marginButtom}`}
-              placeholder='Name'
-              // onChange={handleChange}
-              name='name'
-              // value={name}
-              autoFocus
-              autoComplete='false'
-            ></input>
-            <div className={s.accountPlaceholder}>
-              <div className={s.accountPlaceholder__iconWrapper}>
-                <svg width='21' height='16'>
-                  <use href={`${Icons}#icon-account_box`} />
-                </svg>
-                <span className={s.accountPlaceholder__title}>Name</span>
-              </div>
+          <div>
+            <label className={s.accountLabel}>
+              <input
+                className={s.accountInput}
+                placeholder='E-mail'
+                // onChange={handleChange}
+                name='email'
+                // value={email}
+                autoFocus
+                autoComplete='false'
+              ></input>
               <button className={s.accountBtn__update}>update</button>
-            </div>
-          </label>
-        </div>
-
-        <div>
-          <label className={s.accountLabel}>
-            <input
-              className={s.accountInput}
-              placeholder='E-mail'
-              // onChange={handleChange}
-              name='email'
-              // value={email}
-              autoFocus
-              autoComplete='false'
-            ></input>
-            <div className={s.accountPlaceholder}>
-              <div className={s.accountPlaceholder__iconWrapper}>
-                <svg width='21' height='16'>
-                  <use href={`${Icons}#icon-email`} />
-                </svg>
-                <span className={s.accountPlaceholder__title}>E-mail</span>
+              <div className={s.accountPlaceholder}>
+                <div className={s.accountPlaceholder__iconWrapper}>
+                  <svg width='21' height='16'>
+                    <use href={`${Icons}#icon-email`} />
+                  </svg>
+                  <span className={s.accountPlaceholder__title}>E-mail</span>
+                </div>
               </div>
-              <button className={s.accountBtn__update}>update</button>
-            </div>
-          </label>
-        </div>
+            </label>
+          </div>
 
-        <div className={s.btn__wrapper}>
-          <Button className='btn__primary' type='button' text='try premium' />
-          <Button className='btn__red' type='button' text='delete account' />
+          <div className={s.btn__wrapper}>
+            <Button className='btn__primary' type='button' text='try premium' />
+            <Button className='btn__red' type='button' text='delete account' />
+          </div>
         </div>
       </div>
     </form>
