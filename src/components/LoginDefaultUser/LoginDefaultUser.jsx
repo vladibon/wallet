64efb2b8ce92d7238e8e4a5be364d8a9ba103090
Vladimir Bondar <vladi.bondar@gmail.com>
2022-04-20 23:14:15 +0200

    add LoginDefaultUser component

---
+++ src/components/LoginDefaultUser/LoginDefaultUser.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLogInUserMutation, setAuth, setUser, setError } from 'redux/index';
+import Button from 'components/Button';
+import s from './LoginDefaultUser.module.css';
+
+export default function LoginDefaultUser() {
+  const [loginUser, { data, error, isLoading }] = useLogInUserMutation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (data) {
+      try {
+        dispatch(setAuth({ token: data.token }));
+        dispatch(setUser(data.user));
+      } catch (error) {
+        dispatch(setError(error));
+      }
+    }
+  }, [data, dispatch, error]);
+
+  const onLogin = () => {
+    try {
+      loginUser({ user: { email: 'mango@gmail.com', password: '123456' } });
+    } catch (error) {
+      dispatch(setError(error));
+    }
+  };
+
+  return (
+    <div className={s.buttonContainer}>
+      <Button
+        className='btn__primary'
+        type='button'
+        text='Login as a guest'
+        isLoading={isLoading}
+        onClick={onLogin}
+      />
+    </div>
+  );
+}
