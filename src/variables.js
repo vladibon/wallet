0aa1ecde9c552fcc8dbd898af92515217158878e
Vladimir Bondar <vladi.bondar@gmail.com>
2022-04-27 10:52:31 +0200

    change host to heroku

--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,4 @@
-export const BASE_URL = 'https://wallet-proj.osc-fr1.scalingo.io';
+// export const BASE_URL = 'https://wallet-proj.osc-fr1.scalingo.io';
+export const BASE_URL = 'https://wallet-proj.herokuapp.com';
 
 export const TEST_USER = { email: 'guest@gmail.com', password: 'f8kd8ddld9dd' };
